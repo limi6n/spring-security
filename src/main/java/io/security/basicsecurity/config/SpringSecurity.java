8c6bea7ddfdbe674163cb29d7e564b80e286f130
limi6n <6nlimitless@gmail.com>
2022-01-09 18:12:52 +0900

    antMatcher - 권한설정 및 표현식

--- src/main/java/io/security/basicsecurity/config/SpringSecurity.java
+++ src/main/java/io/security/basicsecurity/config/SpringSecurity.java
@@ -2,6 +2,7 @@ package io.security.basicsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,21 +15,24 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 인메모리 저장으로 사용자 생성하여 테스트
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS","USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN","SYS","ADMIN");
+    }
+
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated()
 
             .and()
                 .formLogin()
-
-            .and()
-                .rememberMe()
-                .userDetailsService(userDetailsService)
-
-            .and()
-                .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true);
+        ;
     }
 }
--- src/main/java/io/security/basicsecurity/web/SecurityController.java
+++ src/main/java/io/security/basicsecurity/web/SecurityController.java
@@ -15,4 +15,19 @@ public class SecurityController {
     public String loginPage() {
         return "loginPage";
     }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "admin-pay";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin-all";
+    }
 }
